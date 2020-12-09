1056903e327eedacd743e2ec8ee2c969b7e69418
Dan Wallace <69432266+wallacewd@users.noreply.github.com>
2020-12-08 20:04:29 -0500

    Commit 1

--- jsonupload.php
+++ jsonupload.php
@@ -1,4 +1,10 @@
 <?php
+                  // JSON File Uploader
+                  // Initially created to upload the test paramaters for multiple stock-trading bots running at the same time.
+                  // You can a json file with anyname and it will automatically re-write a set json file in that directory
+                  // This is useful if you are calling a static json url but need to update the values in the file frequently
+                  // Best used for internal use only or to learn/build off of.
+                  // Dan Wallace
 if ( isset($_POST["submit"]) ) {
 
    if ( isset($_FILES["file"])) {
@@ -11,9 +17,9 @@ if ( isset($_POST["submit"]) ) {
         }else {
                   // Print details
                  
-             echo "Upload: " . $_FILES["file"]["name"] . "<br/>";
-             echo "Type: " . $_FILES["file"]["type"] . "<br/>";
-             echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br/>";
+             echo "File Upload: " . $_FILES["file"]["name"] . "<br/>";
+             echo "File Type: " . $_FILES["file"]["type"] . "<br/>";
+             echo "File Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br/>";
              
                   // Check if the file exists
                  
@@ -21,11 +27,13 @@ if ( isset($_POST["submit"]) ) {
             echo $_FILES["file"]["name"] . " already exists. ";
             
              }else {     
-                  // Save file in folder "upload" 
-                    
+ 
+                  // You can change the name of the static JSON file here  
             $storagename = "json_update.json";
+                
+                  // Save file in folder "upload" 
             move_uploaded_file($_FILES["file"]["tmp_name"], "uploads/" . $storagename);
-            echo "Stored in: " . "upload/" . $_FILES["file"]["name"] . "<br />";
+            echo "File Path: " . "uploads/" . $_FILES["file"]["name"] . "<br />";
             
             }
             
